96b61a4950d343cbecd57a69fa2c662f2c422bb0
Freya Wills <freya.wills@gmail.com>
2020-07-28 17:30:10 +0100

    Connected button to blog post page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome_page, name='welcome_page'),
+    path('post/posts/', views.post_list, name='post_list'),
     #path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
