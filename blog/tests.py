4e7d522a0b18faff4e28e65bd8cf485772e6f14b
Freya Wills <freya.wills@gmail.com>
2020-08-11 17:28:41 +0100

    Added new model for CV items

--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,7 @@ from django.template.loader import render_to_string
 #from django.template.loader import render_to_string
 
 from blog.views import welcome_page  
+from blog.models import Item
 
 class WelcomePageTest(TestCase):
 
@@ -27,3 +28,20 @@ class WelcomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
         self.assertTemplateUsed(response, 'blog/welcome_page.html')"""
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Second item')
